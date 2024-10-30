e98f518af6a0aa7b5f097f4c7cd6c48e21a7ea6c
ditiro1 <109585638+ditiro1@users.noreply.github.com>
2024-10-30 05:26:36 +0200

    Create transmitter.ino

---
+++ transmitter.ino
@@ -0,0 +1,105 @@
+/*************************************************************************************************
+/*  426/433/470MHz LoRa ® Antenna Transceiver Module
+◆ Frequency Range:426/433/470 MHz
+◆ Semtech SX127x Engine
+◆ High efficiency Power Amplifier
+◆ Excellent blocking immunity
+◆ Low Receive current
+◆ High sensitivity
+◆ Control easily by AT commands
+◆ 127 dB Dynamic Range RSSI
+◆ Designed with integrated antenna
+◆ AES128 Data encryption
+◆ Operation temperature: -40℃ to +85℃
+◆ Dimension: 53.5*18.36*5.5 mm
+◆ Weight: 7g
+**************************************************************************************************/
+
+
+/*************************************************************************************************
+ *  Created By: NewEra TEL324 STUDENTS 
+ *  Created On: 23 SEP, 2024
+ *  ANIMAL_TRACKING_SYSTEM 
+ *  LoRa-Blink-TRANSMITTER
+ ***********************************************************************************************
+ *  Install the following libraries :
+ *  1. AltSoftSerial - https://github.com/PaulStoffregen/AltSoftSerial
+ *  2. TinyGPSPlus - Version 1.0.3
+ ***********************************************************************************************/
+
+#include <SoftwareSerial.h>
+#include <AltSoftSerial.h>
+#include <TinyGPSPlus.h>
+
+#define L_RX 3
+#define L_TX 2
+SoftwareSerial LoRaSerial(L_RX, L_TX); // RX, TX
+
+#define G_RX 4
+#define G_TX 5
+AltSoftSerial gpsSerial(G_RX, G_TX); // RX, TX
+
+TinyGPSPlus gps;
+#define BAUDRATE 115200
+
+void setup() {
+  Serial.begin(BAUDRATE); // USB Serial
+  delay(1000);
+  
+  LoRaSerial.begin(BAUDRATE); // LoRa Software Serial
+  gpsSerial.begin(9600); // NEO6M Software Serial
+  
+}
+
+void loop() {
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (gpsSerial.available() > 0) {
+    if (gps.encode(gpsSerial.read())) {
+      displayInfo();
+    }
+  }
+  
+  if (millis() > 5000 && gps.charsProcessed() < 10) {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+  
+}
+
+void displayInfo()
+{
+  if (gps.location.isValid())
+  {
+    String data;
+    
+    data += String(gps.location.lat(), 6);
+    data += ",";
+    data += String(gps.location.lng(), 6);
+    data += "|";
+    
+    char charArray[20];    
+    data.toCharArray(charArray, sizeof(charArray));
+    
+    sendLoraData(data, 2);
+    Serial.println(data);
+    Serial.flush();
+    delay(3000);
+  }
+}
+
+void sendLoraData(String data, int address) {
+  String myString = "AT+SEND=" + String(address) + "," + String(data.length()) + "," + data + "\r\n";
+  char* buf = (char*) malloc(sizeof(char) * myString.length() + 1);
+  //Serial.println(myString);
+  myString.toCharArray(buf, myString.length() + 1);
+  LoRaSerial.write(buf);
+  free(buf);
+}
+
+
+void flushSerialBuffer() {
+  while (Serial.available()) {
+    Serial.read();
+  }
+}
+/*****************************************************************************************************************
